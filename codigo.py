5b6ac61862529e5f5879798978951f9a494e5ac9
lauracattabriga <cattabrigalaura@gmail.com>
2026-03-06 14:05:01 -0300

    Cadastro de produtos - Automação pronta

--- codigo.py
+++ codigo.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pandas
 
 # pyautogui.click -> clica
 # pyautogui.write -> escreve
@@ -20,6 +21,41 @@ time.sleep(3)
 # Clicar no campo de email e senha
 pyautogui.click(x=-1132, y=176)
 pyautogui.write("pythonimpressionador@gmail.com")
-pyautogui.click(x=-1139, y=299)
+pyautogui.press("tab")
 pyautogui.write("123456")
-pyautogui.click(x=-807, y=375)
+pyautogui.press("tab")
+pyautogui.press("enter")
+time.sleep(3)
+
+# Importar a base de dados
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+
+#Cadastrar os produtos
+    pyautogui.click(x=-1155, y=29)
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+    obs = str(tabela.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
